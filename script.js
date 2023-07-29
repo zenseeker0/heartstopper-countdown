f13708c35eba647c4bfa61da9975a9a63592e5a2
zenseeker0 <zenseeker0@gmail.com>
2023-07-28 18:21:05 -0600

    first version of files untested

---
+++ script.js
@@ -0,0 +1,22 @@
+const targetDate = new Date("YYYY-MM-DDThh:mm:ss"); // Replace with your target date and time (e.g., 2023-12-31T23:59:59)
+
+function updateCountdown() {
+    const now = new Date().getTime();
+    const timeRemaining = targetDate - now;
+
+    if (timeRemaining <= 0) {
+        document.getElementById("timer").innerHTML = "Countdown Expired!";
+    } else {
+        const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+        const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+        document.getElementById("timer").innerHTML = `
+            ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds
+        `;
+    }
+}
+
+// Update the countdown every second
+setInterval(updateCountdown, 1000);
