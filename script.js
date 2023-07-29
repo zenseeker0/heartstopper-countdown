dc8116632f15a2fa5da57bab284a0b35c376b139
zenseeker0 <zenseeker0@gmail.com>
2023-07-28 19:33:33 -0600

    remove seconds

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-// Trying to trigger SCM
-
-const targetDate = new Date("2023-08-03T13:00"); // Replace with your target date and time (e.g., 2023-12-31T23:59:59)
+const targetDate = new Date("2023-08-03T13:00:00"); // Replace with your target date and time (e.g., 2023-12-31T23:59:59)
 
 function updateCountdown() {
     const now = new Date().getTime();
@@ -12,10 +10,9 @@ function updateCountdown() {
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
         document.getElementById("timer").innerHTML = `
-            ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds
+            ${days} days, ${hours} hours, ${minutes} minutes
         `;
     }
 }
