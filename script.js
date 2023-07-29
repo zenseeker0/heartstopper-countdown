a5e084777301bca4ac3b24f1fd79bf3d27404fee
zenseeker0 <zenseeker0@gmail.com>
2023-07-28 20:16:49 -0600

    remove text and residual images holders

--- script.js
+++ script.js
@@ -10,9 +10,10 @@ function updateCountdown() {
         const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
         const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
         const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
 
         document.getElementById("timer").innerHTML = `
-            ${days} days, ${hours} hours, ${minutes} minutes
+            ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds
         `;
     }
 }
